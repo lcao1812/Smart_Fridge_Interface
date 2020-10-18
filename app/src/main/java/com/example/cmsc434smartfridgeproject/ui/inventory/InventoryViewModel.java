8f319dafbccd27bda0f85a714a8882e66cb703e2
Kyle Chang <kyle@Kyles-MacBook-Pro.local>
2020-10-18 12:20:42 -0400

    added horizontal prototype of inventory will add better display for items later

--- app/src/main/java/com/example/cmsc434smartfridgeproject/ui/inventory/InventoryViewModel.java
+++ app/src/main/java/com/example/cmsc434smartfridgeproject/ui/inventory/InventoryViewModel.java
@@ -1,16 +1,16 @@
-package com.example.cmsc434smartfridgeproject.ui.dashboard;
+package com.example.cmsc434smartfridgeproject.ui.inventory;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class InventoryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public InventoryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is inventory fragment");
     }
 
     public LiveData<String> getText() {
