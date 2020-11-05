94312085d0caabfb52be8961202712df4394170d
Crystal <cryswang@terpmail.umd.edu>
2020-11-04 20:23:48 -0500

    ugly but functional, commented out settings and much of recipes functionality

--- app/src/main/java/com/example/cmsc434smartfridgeproject/ui/recipes/RecipesFragment.java
+++ app/src/main/java/com/example/cmsc434smartfridgeproject/ui/recipes/RecipesFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.cmsc434smartfridgeproject.R;
 import com.example.cmsc434smartfridgeproject.ui.recipes.RecipesFragment;
+import com.example.cmsc434smartfridgeproject.ui.recipes.RecipesItemFragment;
 import com.example.cmsc434smartfridgeproject.ui.recipes.RecipesViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -42,20 +43,22 @@ public class RecipesFragment extends Fragment {
         recipeList.add("Week of Sept 2nd");
         recipeList.add("This is an example scenario -- Number 1");
 
-        rList = (ListView) book.findViewById(R.id.itemList);
+        System.out.println(recipeList.size());
+
+        rList = (ListView) book.findViewById(R.id.recipeList);
         adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, recipeList);
         rList.setAdapter(adapter);
 
         // action for the fab button
-        add_fab = (FloatingActionButton) book.findViewById(R.id.fab_add);
-        add_fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Function to add Item here
-                addRecipe();
-                // ****************************
-            }
-        });
+//        add_fab = (FloatingActionButton) book.findViewById(R.id.fab_add);
+//        add_fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Function to add Item here
+//                addRecipe();
+//                // ****************************
+//            }
+//        });
 
         rList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
@@ -70,41 +73,41 @@ public class RecipesFragment extends Fragment {
                 Bundle bundle = new Bundle();
 
                 FragmentManager manager = getFragmentManager();
-//                manager.beginTransaction().replace(R.id.nav_host_fragment, recipesItemFragment, recipesItemFragment.getTag()).commit();
+//              manager.beginTransaction().replace(R.id.nav_host_fragment, recipesItemFragment, recipesItemFragment.getTag()).commit();
             }
         });
 
         return book;
     }
 
-    private void addRecipe() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Add New Item");
-
-        View v = LayoutInflater.from(getActivity()).inflate(R.layout.item_dialog, null, false);
-
-        builder.setView(v);
-        final EditText etItem = v.findViewById(R.id.etItem);
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (!etItem.getText().toString().isEmpty()) {
-                    recipeList.add(etItem.getText().toString().trim());
-                    adapter.notifyDataSetChanged();
-                    Toast.makeText(getContext() , "recipe added", Toast.LENGTH_SHORT).show();
-                } else {
-                    etItem.setError("add item here !");
-                }
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
-    }
+//    private void addRecipe() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setTitle("Add New Recipe");
+//
+//        View v = LayoutInflater.from(getActivity()).inflate(R.layout.item_dialog, null, false);
+//
+//        builder.setView(v);
+//        final EditText etItem = v.findViewById(R.id.etItem);
+//        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                if (!etItem.getText().toString().isEmpty()) {
+//                    recipeList.add(etItem.getText().toString().trim());
+//                    adapter.notifyDataSetChanged();
+//                    Toast.makeText(getContext() , "recipe added", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    etItem.setError("add item here !");
+//                }
+//            }
+//        });
+//
+//        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        builder.show();
+//    }
 }
